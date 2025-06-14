0711e4339489b38b221edc4121768930f94c1870
B P <bp@Mac.attlocal.net>
2025-06-13 18:53:43 -0700

    Updated supabase keys.

--- flask-backend/database/supabase_client.py
+++ flask-backend/database/supabase_client.py
@@ -2,8 +2,8 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-load_dotenv('/Users/bp/Documents/crypto_site/crypto-site/.env.local')
-SUPABASE_URL = os.getenv('NEXT_PUBLIC_SUPABASE_URL')
-SUPABASE_KEY = os.getenv('NEXT_PUBLIC_SUPABASE_ANON_KEY')
+load_dotenv('/Users/bp/Documents/crypto_site/crypto-site/flask-backend/venv/keys.env')
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_SECRET_KEY')
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
